e067dafc38eba85e2377431616aa8b33459b2e69
Lance Janssen <lance.janssen@kognity.com>
2025-12-06 23:58:59 +0100

    day 1

---
+++ 2025/day-1-part-1.py
@@ -0,0 +1,77 @@
+from enum import IntEnum
+
+INPUT_FILE = "./day-1-input.txt"
+DIAL_MAX = 100
+
+
+class Direction(IntEnum):
+    LEFT = -1
+    RIGHT = 1
+
+
+class Rotation:
+    def __init__(self, distance: int, direction: Direction):
+        self.distance = distance
+        self.direction = direction
+
+    def diff(self):
+        return self.distance * self.direction
+
+    @staticmethod
+    def parse(text: str):
+        distance = int(text[1:]) if text[1:].isdigit() else None
+        direction = (
+            (Direction.LEFT if text[0] == "L" else Direction.RIGHT)
+            if text[0] in ["L", "R"]
+            else None
+        )
+
+        if distance is None or direction is None:
+            return InvalidRotation(
+                "Invalid Rotation, Expected format <L|R><int> "
+                "(e.g. L12, R46), received: " + text
+            )
+
+        return Rotation(distance, direction)
+
+
+class InvalidRotation(Rotation):
+    def __init__(self, error: str):
+        super().__init__(0, Direction.LEFT)
+        self.error = error
+
+
+class Dial:
+    value: int
+    maximum: int
+
+    def __init__(self, value: int, maximum: Direction):
+        self.value = value
+        self.maximum = maximum
+
+    def rotate(self, rotation: Rotation):
+        self.value = (self.value + rotation.diff()) % self.maximum
+        return self
+
+
+def main():
+    dial = Dial(DIAL_MAX / 2, DIAL_MAX)
+    nrOfZeros = 0
+
+    with open(INPUT_FILE, "r") as file:
+        for line in file:
+            rotation = Rotation.parse(line.strip())
+
+            if isinstance(rotation, InvalidRotation):
+                print("Skipped " + rotation.error)
+
+            dial.rotate(rotation)
+
+            if dial.value == 0:
+                nrOfZeros += 1
+
+    print("Number of zeros: " + str(nrOfZeros))
+
+
+if __name__ == "__main__":
+    main()
---
+++ 2025/day-1-part-2.py
@@ -0,0 +1,86 @@
+import math
+from enum import IntEnum
+
+INPUT_FILE = "./day-1-input.txt"
+DIAL_MAX = 100
+
+
+class Direction(IntEnum):
+    LEFT = -1
+    RIGHT = 1
+
+
+class Rotation:
+    def __init__(self, distance: int, direction: Direction):
+        self.distance = distance
+        self.direction = direction
+
+    def diff(self):
+        return self.distance * self.direction
+
+    @staticmethod
+    def parse(text: str):
+        distance = int(text[1:]) if text[1:].isdigit() else None
+        direction = (
+            (Direction.LEFT if text[0] == "L" else Direction.RIGHT)
+            if text[0] in ["L", "R"]
+            else None
+        )
+
+        if distance is None or direction is None:
+            return InvalidRotation(
+                "Invalid Rotation, Expected format <L|R><int> "
+                "(e.g. L12, R46), received: " + text
+            )
+
+        return Rotation(distance, direction)
+
+
+class InvalidRotation(Rotation):
+    def __init__(self, error: str):
+        super().__init__(0, Direction.LEFT)
+        self.error = error
+
+
+class Dial:
+    value: int
+    maximum: int
+
+    def __init__(self, value: int, maximum: Direction):
+        self.value = value
+        self.maximum = maximum
+
+    def rotate(self, rotation: Rotation):
+        oldValue = self.value
+        unnormalizedNewValue = oldValue + rotation.diff()
+        self.value = unnormalizedNewValue % self.maximum
+
+        fullRotations = math.floor(abs(unnormalizedNewValue) / 100)
+
+        rotationsPastZero = (
+            fullRotations + 1  # additional rotation to zero / negative
+            if unnormalizedNewValue <= 0 and oldValue > 0
+            else fullRotations
+        )
+
+        return rotationsPastZero
+
+
+def main():
+    dial = Dial(DIAL_MAX / 2, DIAL_MAX)
+    rotationsPastZero = 0
+
+    with open(INPUT_FILE, "r") as file:
+        for line in file:
+            rotation = Rotation.parse(line.strip())
+
+            if isinstance(rotation, InvalidRotation):
+                print("Skipped " + rotation.error)
+
+            rotationsPastZero += dial.rotate(rotation)
+
+    print("Rotations past zero: " + str(rotationsPastZero))
+
+
+if __name__ == "__main__":
+    main()
